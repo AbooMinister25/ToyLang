7067152a1fa46d9f0caf67dfba1d95b942157fd1
Rayyan Cyclegar <aboominister@gmail.com>
2020-10-31 12:55:26 -0400

    new commit

--- lite/main.py
+++ lite/main.py
@@ -1,26 +1,33 @@
-from lark import Lark, Transformer
+from lark import Lark, Transformer, v_args
 
-
-parser = Lark.open('lite\lite_parser.lark', parser="lalr", start="start")
-
-
+@v_args(inline=True)
 class LiteTransformer(Transformer):
-    def sum(self, args):
-        return int(args[0]) + int(args[1])
-
-    def sub(self, args):
-        return int(args[0]) - int(args[1])
-
-    def mul(self, args):
-        return int(args[0]) * int(args[1])
-
-    def div(self, args):
-        return int(args[0]) / int(args[1])
+    from operator import add, sub, mul, truediv as div, neg
+    number = float
     
+    def __init__(self):
+        self.vars = {}
+    
+    def assign_var(self, name, value):
+        self.vars[name] = value
+    
+    def var(self, name):
+        try:
+            return self.vars[name]
+        except KeyError:
+            raise Exception(f"Variable {name} not found")
+    
+    def print(self, value):
+        print(value)
+        
+
+parser = Lark.open('lite\lite_parser.lark', parser="lalr", transformer=LiteTransformer())
+
 
 if __name__ == '__main__':
     while True:
-        data = input("> ")
-        tree = parser.parse(data)
-        print(LiteTransformer().transform(tree))
-        
+        try:
+            x = input("> ")
+        except EOFError:
+            break
+        print(parser.parse(x))
