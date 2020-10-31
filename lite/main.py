a32d28a9a75efe56ba90f3528ba3f00887e9b3d8
Rayyan Cyclegar <aboominister@gmail.com>
2020-10-31 18:23:48 -0400

    New commit

--- lite/main.py
+++ lite/main.py
@@ -1,34 +1,37 @@
 from lark import Lark, Transformer, v_args
 
-@v_args(inline=True)
+
+@v_args(inline=True)    # Affects the signatures of the methods
 class LiteTransformer(Transformer):
     from operator import add, sub, mul, truediv as div, neg
     number = float
-    
+
     def __init__(self):
         self.vars = {}
-    
+
     def assign_var(self, name, value):
         self.vars[name] = value
-    
+        return value
+
     def var(self, name):
         try:
             return self.vars[name]
         except KeyError:
             raise Exception(f"Variable {name} not found")
     
-    # def print(self, value):
-    #     print(value)
-        
+    def print(self, value):
+        print(value)
 
-parser = Lark.open('lite\lite_parser.lark', parser="lalr", start="start")
+
+parser = Lark.open('lite\lite_parser.lark', parser="lalr",
+                   transformer=LiteTransformer())
+lite_parser = parser.parse
 
 
 if __name__ == '__main__':
     while True:
         try:
             x = input("> ")
-            tree = parser.parse(x)
-            print(LiteTransformer().transform(tree))
         except EOFError:
             break
+        print(lite_parser(x))
